cb78db8fb61727018c651ef041cd0b97275b1672
Mirzobakhtiyor Rahmatov <ehyozamon@gmail.com>
2025-07-16 03:21:51 +0500

    Загрузка страницы блога

---
+++ index.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Блог о веб-разработке и технологиях </title>
+  <meta name="description" content="Последние статьи о веб-разработке, технологиях, дизайне и маркетинге">
+  
+  <!-- Open Graph / Social Media -->
+  <meta property="og:title" content="Блог о веб-разработке и технологиях ">
+  <meta property="og:description" content="Последние статьи о веб-разработке, технологиях, дизайне и маркетинге">
+  <meta property="og:type" content="website">
+  <meta property="og:locale" content="en_EN">
+  
+  <!-- Favicon -->
+  <link rel="icon" href="favicon.ico" type="image/x-icon">
+  <link rel="apple-touch-icon" href="/images/apple-touch-icon.png">
+  
+  <!-- CSS -->
+  <link rel="stylesheet" href="css/blog.css?v=<?= time(); ?>">
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;600;700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+    <section class="main-block">
+        <div class="main-content">
+            <section class="content-block">
+                <h1>Blog</h1>
+                <nav class="blog-nav">
+                    <div class="blog-nav-elem blog-nav-elem--active">
+                        <p>All</p>
+                    </div>
+                    <div class="blog-nav-elem">
+                        <p>Technology</p>
+                    </div>
+                    <div class="blog-nav-elem">
+                        <p>Design</p>
+                    </div>
+                    <div class="blog-nav-elem">
+                        <p>Marketing</p>
+                    </div>
+                    <div class="blog-nav-elem">
+                        <p>SEO</p>
+                    </div>
+                    <div class="blog-nav-elem">
+                        <p>Development</p>
+                    </div>
+                </nav>
+                
+                <section class="article-group">
+                    <div class="technology-block">
+                        <div class="technology-block-banner">
+                            
+                        </div>
+                        <div class="technology-block-content">
+                            <h3>
+                                Technology
+                            </h3>
+                            
+                            <h2>
+                                Development Technologies That Are Transforming the Industry: Trends, Tools & the Future
+                            </h2>
+                            
+                            <p>We explore which technologies are shaping the future of development, how new tools simplify programmers’ work, improve code quality, and accelerate processes. Which trends are here to stay, and what will become a thing of the past?</p>
+                            
+                            <p class="tech-block-content-date">5 March  2025</p>
+                        </div>
+                    </div>
+                    
+                    <div class="articles">
+                        <figure>
+                            <div class="img-container">
+                                <img src="./images/grey-bg.png"/>
+                            </div>
+                            <figcaption>
+                                <span class="article-name">Technology</span>
+                                <h3>Development Technologies That Are Transforming the Industry: Trends, Tools & the Future</h3>
+                                <span class="article-date">5 March 2025</span>
+                            </figcaption>
+                        </figure>
+                        
+                        <figure>
+                            <div class="img-container">
+                                <img src="./images/grey-bg.png"/>
+                            </div>
+                            <figcaption>
+                                <span class="article-name">Technology</span>
+                                <h3>Development Technologies That Are Transforming the Industry: Trends, Tools & the Future</h3>
+                                <span class="article-date">5 March 2025</span>
+                            </figcaption>
+                        </figure>
+                        
+                        <figure>
+                            <div class="img-container">
+                                <img src="./images/grey-bg.png"/>
+                            </div>
+                            <figcaption>
+                                <span class="article-name">Technology</span>
+                                <h3>Development Technologies That Are Transforming the Industry: Trends, Tools & the Future</h3>
+                                <span class="article-date">5 March 2025</span>
+                            </figcaption>
+                        </figure>
+                        
+                        <figure>
+                            <div class="img-container">
+                                <img src="./images/grey-bg.png"/>
+                            </div>
+                            <figcaption>
+                                <span class="article-name">Technology</span>
+                                <h3>Development Technologies That Are Transforming the Industry: Trends, Tools & the Future</h3>
+                                <span class="article-date">5 March 2025</span>
+                            </figcaption>
+                        </figure>
+                        
+                        <figure>
+                            <div class="img-container">
+                                <img src="./images/grey-bg.png"/>
+                            </div>
+                            <figcaption>
+                                <span class="article-name">Technology</span>
+                                <h3>Development Technologies That Are Transforming the Industry: Trends, Tools & the Future</h3>
+                                <span class="article-date">5 March 2025</span>
+                            </figcaption>
+                        </figure>
+                        
+                        <figure>
+                            <div class="img-container">
+                                <img src="./images/grey-bg.png"/>
+                            </div>
+                            <figcaption>
+                                <span class="article-name">Technology</span>
+                                <h3>Development Technologies That Are Transforming the Industry: Trends, Tools & the Future</h3>
+                                <span class="article-date">5 March 2025</span>
+                            </figcaption>
+                        </figure>
+                    </div>
+                </section>
+                
+                
+                <section class="article-group">
+                    <div class="technology-block">
+                        <div class="technology-block-banner">
+                            
+                        </div>
+                        <div class="technology-block-content">
+                            <h3>
+                                Technology
+                            </h3>
+                            
+                            <h2>
+                                Development Technologies That Are Transforming the Industry: Trends, Tools & the Future
+                            </h2>
+                            
+                            <p>We explore which technologies are shaping the future of development, how new tools simplify programmers’ work, improve code quality, and accelerate processes. Which trends are here to stay, and what will become a thing of the past?</p>
+                            
+                            <p class="tech-block-content-date">5 March  2025</p>
+                        </div>
+                    </div>
+                    
+                    <div class="articles">
+                        <figure>
+                            <div class="img-container">
+                                <img src="./images/grey-bg.png"/>
+                            </div>
+                            <figcaption>
+                                <span class="article-name">Technology</span>
+                                <h3>Development Technologies That Are Transforming the Industry: Trends, Tools & the Future</h3>
+                                <span class="article-date">5 March 2025</span>
+                            </figcaption>
+                        </figure>
+                        
+                        <figure>
+                            <div class="img-container">
+                                <img src="./images/grey-bg.png"/>
+                            </div>
+                            <figcaption>
+                                <span class="article-name">Technology</span>
+                                <h3>Development Technologies That Are Transforming the Industry: Trends, Tools & the Future</h3>
+                                <span class="article-date">5 March 2025</span>
+                            </figcaption>
+                        </figure>
+                        
+                        <figure>
+                            <div class="img-container">
+                                <img src="./images/grey-bg.png"/>
+                            </div>
+                            <figcaption>
+                                <span class="article-name">Technology</span>
+                                <h3>Development Technologies That Are Transforming the Industry: Trends, Tools & the Future</h3>
+                                <span class="article-date">5 March 2025</span>
+                            </figcaption>
+                        </figure>
+                        
+                        <figure>
+                            <div class="img-container">
+                                <img src="./images/grey-bg.png"/>
+                            </div>
+                            <figcaption>
+                                <span class="article-name">Technology</span>
+                                <h3>Development Technologies That Are Transforming the Industry: Trends, Tools & the Future</h3>
+                                <span class="article-date">5 March 2025</span>
+                            </figcaption>
+                        </figure>
+                        
+                        <figure>
+                            <div class="img-container">
+                                <img src="./images/grey-bg.png"/>
+                            </div>
+                            <figcaption>
+                                <span class="article-name">Technology</span>
+                                <h3>Development Technologies That Are Transforming the Industry: Trends, Tools & the Future</h3>
+                                <span class="article-date">5 March 2025</span>
+                            </figcaption>
+                        </figure>
+                        
+                        <figure>
+                            <div class="img-container">
+                                <img src="./images/grey-bg.png"/>
+                            </div>
+                            <figcaption>
+                                <span class="article-name">Technology</span>
+                                <h3>Development Technologies That Are Transforming the Industry: Trends, Tools & the Future</h3>
+                                <span class="article-date">5 March 2025</span>
+                            </figcaption>
+                        </figure>
+                    </div>
+                </section>
+              
+                <div class="pagination-container">
+                  <button class="show-more">Show 10 more</button>
+                  
+                  <div class="pagination">
+                    <button class="page active">1</button>
+                    <button class="page">2</button>
+                    <button class="page">3</button>
+                    <button class="page">4</button>
+                    <button class="page">5</button>
+                    <button class="next">Вперёд →</button>
+                  </div>
+                </div>  
+            </section>
+        </div>
+    </section>
+</body>
+</html>
