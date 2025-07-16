e146af017ccb2080f0d7442c41f528a5104c5664
Mirzobakhtiyor Rahmatov <ehyozamon@gmail.com>
2025-07-17 01:25:41 +0500

    Вёрстка блока страницы услуг

---
+++ services.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Услуги по веб-разработке</title>
+  <meta name="description" content="Услуги по веб-разработке, технологиях, дизайне и маркетинге">
+  
+  <!-- Open Graph / Social Media -->
+  <meta property="og:title" content="Услуги по веб-разработке">
+  <meta property="og:description" content="Услуги по веб-разработке, технологиях, дизайне и маркетинге">
+  <meta property="og:type" content="website">
+  <meta property="og:locale" content="en_EN">
+  
+  <!-- Favicon -->
+  <link rel="icon" href="favicon.ico" type="image/x-icon">
+  <link rel="apple-touch-icon" href="/images/apple-touch-icon.png">
+  
+  <!-- CSS -->
+  <link rel="stylesheet" href="css/services.css?v=<?= time(); ?>">
+  <!-- Google Fonts: Manrope, Golos Text, Inter -->
+  <link href="https://fonts.googleapis.com/css2?family=Golos+Text&family=Inter&family=Manrope&display=swap" rel="stylesheet">
+
+  <!-- Fontshare: NT Somic -->
+  <link href="https://api.fontshare.com/v2/css?f[]=nt-somic@400&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+    <section class="main-block">
+        <div class="main-content">
+            <section class="content-block">
+                <div class="services-container">
+                    <div class="services-page-header">
+                        <h1>Услуги</h1>
+                    </div>
+                    <div class="services-cards">
+                        <div class="service-card">
+                            <div class="service-card-banner">
+                                <img src="./images/services/layer1.webp" alt="service-layer 1" class="service-card-banner-img"/>
+                            </div>
+                            <div class="service-card-content">
+                                <div class="service-card-content-container">
+                                    <h2>SEO</h2>
+                                    <p>Оптимизируем сайт для поисковых систем Google, Bin, Yandex, увеличиваем видимость  и привлекаем трафик.</p>
+                                </div>
+                                <a href="#" class="service-card-link"><img src="./images/services/arrow.svg" alt="arrow"/></a>
+                            </div>
+                        </div>
+                        
+                        <div class="service-card">
+                            <div class="service-card-banner">
+                                <img src="./images/services/layer2.webp" alt="service-layer 1" class="service-card-banner-img"/>
+                            </div>
+                            <div class="service-card-content">
+                                <div class="service-card-content-container">
+                                    <h2>UX/UI design</h2>
+                                    <p>Создаем удобный, стильный и логичный интерфейс, который улучшает пользовательский опыт.</p>
+                                </div>
+                                <a href="#" class="service-card-link"><img src="./images/services/arrow.svg" alt="arrow"/></a>
+                            </div>
+                        </div>
+                        
+                        <div class="service-card">
+                            <div class="service-card-banner">
+                                <img src="./images/services/layer3.webp" alt="service-layer 1" class="service-card-banner-img"/>
+                            </div>
+                            <div class="service-card-content">
+                                <div class="service-card-content-container">
+                                    <h2>Development</h2>
+                                    <p>Превращаем дизайн в работающий сайт с быстрой загрузкой, адаптивностью и нужным функционалом.</p>
+                                </div>
+                                <a href="#" class="service-card-link"><img src="./images/services/arrow.svg" alt="arrow"/></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </section>
+</body>
+</html>
