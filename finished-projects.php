55ea1d20c0f125ae4f5f9d9676a326c5361ccd25
Mirzobakhtiyor Rahmatov <ehyozamon@gmail.com>
2025-07-18 11:49:09 +0500

    Вёрстка страницы проекты

---
+++ finished-projects.php
@@ -0,0 +1,318 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Блог о веб-разработке и технологиях </title>
+  <meta name="description" content="Последние статьи о веб-разработке, технологиях, дизайне и маркетинге">
+  
+  <!-- Open Graph / Social Media -->
+  <meta property="og:title" content="Блог о веб-разработке и технологиях ">
+  <meta property="og:description" content="Последние статьи о веб-разработке, технологиях, дизайне и маркетинге">
+  <meta property="og:type" content="website">
+  <meta property="og:locale" content="en_EN">
+  
+  <!-- Favicon -->
+  <link rel="icon" href="favicon.ico" type="image/x-icon">
+  <link rel="apple-touch-icon" href="/images/apple-touch-icon.png">
+  
+  <!-- CSS -->
+  <link rel="stylesheet" href="css/finished-projects.css?v=<?= time(); ?>">
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;600;700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+    <section class="main-block">
+        <div class="main-content">
+            <section class="content-block">
+                <h1>Блог</h1>
+                <nav class="projects-nav">
+                    <div class="projects-nav-elem projects-nav-elem--active">
+                        <p>Все</p>
+                    </div>
+                    <div class="projects-nav-elem">
+                        <p>SEO</p>
+                    </div>
+                    <div class="projects-nav-elem">
+                        <p>Development & UX/UI design</p>
+                    </div>
+                </nav>
+                
+                <section class="article-group">
+                    <div class="articles two-articles">
+                        <article>
+                            <a href="#" class="article-link">
+                                <figure>
+                                    <div href="#" class="img-container first-img-container">
+                                        <img src="./images/technologies/tech2.webp"/>
+                                        <div class="img-inner-block">
+                                            <div class="categories-block">
+                                                <div class="category-container">SEO</div>
+                                                <div class="category-container">UX/UI</div>
+                                                <div class="category-container">DEV</div>
+                                            </div>
+                                            <div class="arrow-link">
+                                                <img src="./images/icons/right-up.svg" alt="arrow"/>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <figcaption>
+                                        <div class="article-name">Сфера Автоматики</div>
+                                        <div class="article-description">Структуризация сайта и внутренняя оптимизация</div>
+                                    </figcaption>
+                                </figure>
+                            </a>
+                        </article>
+                        
+                        <article>
+                            <a href="#" class="article-link">
+                                <figure>
+                                    <div href="#" class="img-container second-img-container">
+                                        <img src="./images/technologies/tech3.webp"/>
+                                        <div class="img-inner-block">
+                                            <div class="categories-block">
+                                                <div class="category-container">SEO</div>
+                                                <div class="category-container">UX/UI</div>
+                                                <div class="category-container">DEV</div>
+                                            </div>
+                                            <div class="arrow-link">
+                                                <img src="./images/icons/right-up.svg" alt="arrow"/>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <figcaption>
+                                        <div class="article-name">Bazaar</div>
+                                        <div class="article-description">Структуризация сайта и внутренняя оптимизация</div>
+                                    </figcaption>
+                                </figure>
+                            </a>
+                        </article>
+                    </div>
+                    
+                    <div class="articles">
+                        <article>
+                            <a href="#" class="article-link">
+                                <figure>
+                                    <div href="#" class="img-container simple-img-container">
+                                        <img src="./images/technologies/tech2.webp"/>
+                                        <div class="img-inner-block">
+                                            <div class="categories-block">
+                                                <div class="category-container">SEO</div>
+                                                <div class="category-container">UX/UI</div>
+                                                <div class="category-container">DEV</div>
+                                            </div>
+                                            <div class="arrow-link">
+                                                <img src="./images/icons/right-up.svg" alt="arrow"/>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <figcaption>
+                                        <div class="article-name">Сфера Автоматики</div>
+                                        <div class="article-description">Структуризация сайта и внутренняя оптимизация</div>
+                                    </figcaption>
+                                </figure>
+                            </a>
+                        </article>
+                        
+                        <article>
+                            <a href="#" class="article-link">
+                                <figure>
+                                    <div href="#" class="img-container simple-img-container">
+                                        <img src="./images/technologies/tech3.webp"/>
+                                        <div class="img-inner-block">
+                                            <div class="categories-block">
+                                                <div class="category-container">SEO</div>
+                                                <div class="category-container">UX/UI</div>
+                                                <div class="category-container">DEV</div>
+                                            </div>
+                                            <div class="arrow-link">
+                                                <img src="./images/icons/right-up.svg" alt="arrow"/>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <figcaption>
+                                        <div class="article-name">Bazaar</div>
+                                        <div class="article-description">Структуризация сайта и внутренняя оптимизация</div>
+                                    </figcaption>
+                                </figure>
+                            </a>
+                        </article>
+                        
+                        <article>
+                            <a href="#" class="article-link">
+                                <figure>
+                                    <div href="#" class="img-container simple-img-container">
+                                        <img src="./images/technologies/tech4.webp"/>
+                                        <div class="img-inner-block">
+                                            <div class="categories-block">
+                                                <div class="category-container">SEO</div>
+                                                <div class="category-container">UX/UI</div>
+                                                <div class="category-container">DEV</div>
+                                            </div>
+                                            <div class="arrow-link">
+                                                <img src="./images/icons/right-up.svg" alt="arrow"/>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <figcaption>
+                                        <div class="article-name">El Mundo Bordado</div>
+                                        <div class="article-description">Структуризация сайта и внутренняя оптимизация</div>
+                                    </figcaption>
+                                </figure>
+                            </a>
+                        </article>
+                    </div>
+                </section>
+   
+   
+   
+   
+   
+                
+                <section class="article-group">
+                    <div class="articles two-articles">
+                        <article>
+                            <a href="#" class="article-link">
+                                <figure>
+                                    <div href="#" class="img-container first-img-container">
+                                        <img src="./images/technologies/tech2.webp"/>
+                                        <div class="img-inner-block">
+                                            <div class="categories-block">
+                                                <div class="category-container">SEO</div>
+                                                <div class="category-container">UX/UI</div>
+                                                <div class="category-container">DEV</div>
+                                            </div>
+                                            <div class="arrow-link">
+                                                <img src="./images/icons/right-up.svg" alt="arrow"/>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <figcaption>
+                                        <div class="article-name">Сфера Автоматики</div>
+                                        <div class="article-description">Структуризация сайта и внутренняя оптимизация</div>
+                                    </figcaption>
+                                </figure>
+                            </a>
+                        </article>
+                        
+                        <article>
+                            <a href="#" class="article-link">
+                                <figure>
+                                    <div href="#" class="img-container second-img-container">
+                                        <img src="./images/technologies/tech3.webp"/>
+                                        <div class="img-inner-block">
+                                            <div class="categories-block">
+                                                <div class="category-container">SEO</div>
+                                                <div class="category-container">UX/UI</div>
+                                                <div class="category-container">DEV</div>
+                                            </div>
+                                            <div class="arrow-link">
+                                                <img src="./images/icons/right-up.svg" alt="arrow"/>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <figcaption>
+                                        <div class="article-name">Bazaar</div>
+                                        <div class="article-description">Структуризация сайта и внутренняя оптимизация</div>
+                                    </figcaption>
+                                </figure>
+                            </a>
+                        </article>
+                    </div>
+                    
+                    <div class="articles">
+                        <article>
+                            <a href="#" class="article-link">
+                                <figure>
+                                    <div href="#" class="img-container simple-img-container">
+                                        <img src="./images/technologies/tech2.webp"/>
+                                        <div class="img-inner-block">
+                                            <div class="categories-block">
+                                                <div class="category-container">SEO</div>
+                                                <div class="category-container">UX/UI</div>
+                                                <div class="category-container">DEV</div>
+                                            </div>
+                                            <div class="arrow-link">
+                                                <img src="./images/icons/right-up.svg" alt="arrow"/>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <figcaption>
+                                        <div class="article-name">Сфера Автоматики</div>
+                                        <div class="article-description">Структуризация сайта и внутренняя оптимизация</div>
+                                    </figcaption>
+                                </figure>
+                            </a>
+                        </article>
+                        
+                        <article>
+                            <a href="#" class="article-link">
+                                <figure>
+                                    <div href="#" class="img-container simple-img-container">
+                                        <img src="./images/technologies/tech3.webp"/>
+                                        <div class="img-inner-block">
+                                            <div class="categories-block">
+                                                <div class="category-container">SEO</div>
+                                                <div class="category-container">UX/UI</div>
+                                                <div class="category-container">DEV</div>
+                                            </div>
+                                            <div class="arrow-link">
+                                                <img src="./images/icons/right-up.svg" alt="arrow"/>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <figcaption>
+                                        <div class="article-name">Bazaar</div>
+                                        <div class="article-description">Структуризация сайта и внутренняя оптимизация</div>
+                                    </figcaption>
+                                </figure>
+                            </a>
+                        </article>
+                        
+                        <article>
+                            <a href="#" class="article-link">
+                                <figure>
+                                    <div href="#" class="img-container simple-img-container">
+                                        <img src="./images/technologies/tech4.webp"/>
+                                        <div class="img-inner-block">
+                                            <div class="categories-block">
+                                                <div class="category-container">SEO</div>
+                                                <div class="category-container">UX/UI</div>
+                                                <div class="category-container">DEV</div>
+                                            </div>
+                                            <div class="arrow-link">
+                                                <img src="./images/icons/right-up.svg" alt="arrow"/>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <figcaption>
+                                        <div class="article-name">El Mundo Bordado</div>
+                                        <div class="article-description">Структуризация сайта и внутренняя оптимизация</div>
+                                    </figcaption>
+                                </figure>
+                            </a>
+                        </article>
+                    </div>
+                </section>
+                
+                <div class="pagination-container">
+                    <div class="show-more-container">
+                        <a href="#" class="show-more">Показать ещё 10</a>
+                    </div>
+                    
+                  <div class="pagination">
+                    <a href="#" class="page active">1</a>
+                    <a href="#" class="page">2</a>
+                    <a href="#" class="page">3</a>
+                    <a href="#" class="page">4</a>
+                    <a href="#" class="page">5</a>
+                    <a href="#" class="next"><span class="not-for-mobile">Вперёд </span>→</a>
+                  </div>
+                </div>  
+            </section>
+        </div>
+    </section>
+</body>
+</html>
